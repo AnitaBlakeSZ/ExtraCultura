a74470d1e178fbae49ffccf4fb32fd666d8f3482
AnitaBlakeSZ <sofiazuffiab@gmail.com>
2022-04-26 00:22:42 +0200

    photo description changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,50 +1,14 @@
 $(function(){
-    //gestione descrizione sulle immagini
-    $("#uno").hover(function() {
-        $(this).css("display", "none");
-        $("#uno_t").css("display", "inline");
-    });
-    $("#uno_t").hover(function() {}, function() {
-        $(this).css("display", "none");
-        $("#uno").css("display", "inline");
-    });
 
-    $("#due").hover(function() {
-        $(this).css("display", "none");
-        $("#due_t").css("display", "inline");
-    });
-    $("#due_t").hover(function() {}, function() {
-        $(this).css("display", "none");
-        $("#due").css("display", "inline");
-    });
-
-    $("#tre").hover(function() {
-        $(this).css("display", "none");
-        $("#tre_t").css("display", "inline");
-    });
-    $("#tre_t").hover(function() {}, function() {
-        $(this).css("display", "none");
-        $("#tre").css("display", "inline");
-    });
-
-    $("#quattro").hover(function() {
-        $(this).css("display", "none");
-        $("#quattro_t").css("display", "inline");
-    });
-    $("#quattro_t").hover(function() {}, function() {
-        $(this).css("display", "none");
-        $("#quattro").css("display", "inline");
-    });
-
-    $("#cinque").hover(function() {
-        $(this).css("display", "none");
-        $("#cinque_t").css("display", "inline");
-    });
-    $("#cinque_t").hover(function() {}, function() {
-        $(this).css("display", "none");
-        $("#cinque").css("display", "inline");
-    });
-
+    $(".container").hover(
+        function() {
+            $(".foto_descr").css("opacity", "0.3");
+            $(".descrizione").css("display", "flex");
+        }, function() {
+            $(".foto_descr").css("opacity", 1);
+            $(".descrizione").css("display", "none");
+        }
+    );
     //gestione navbar
     $(".menu_1").on("click", function() {
         $(".destra").css("display", "flex");
